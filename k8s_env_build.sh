a7b5637940e77e2c9ab5dcbed2a3061a8f308a69
jybyun <jybyun@osci.kr>
2025-02-13 15:56:14 +0900

    kubernetes vagrant

---
+++ k8s_env_build.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+# vim configuration 
+echo 'alias vi=vim' >> /etc/profile
+
+# swapoff -a to disable swapping
+swapoff -a
+# sed to comment the swap partition in /etc/fstab
+sed -i.bak -r 's/(.+ swap .+)/#\1/' /etc/fstab
+
+# config DNS  
+cat <<EOF > /etc/resolv.conf
+nameserver 1.1.1.1 #cloudflare DNS
+nameserver 8.8.8.8 #Google DNS
+EOF
+
+# CentOS repo 
+mkdir /etc/yum.repos.d/repo_backup
+mv /etc/yum.repos.d/*.repo /etc/yum.repos.d/repo_backup/
+
+cat <<EOF | sudo tee /etc/yum.repos.d/CentOS.repo
+[base]
+name=CentOS-$releasever - Base
+baseurl=https://vault.centos.org/7.9.2009/os/x86_64/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+[updates]
+name=CentOS-$releasever - Updates
+baseurl=https://vault.centos.org/7.9.2009/updates/x86_64/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+ 
+
+[extras]
+name=CentOS-$releasever - Extras
+baseurl=https://vault.centos.org/7.9.2009/extras/x86_64/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+[centosplus]
+name=CentOS-$releasever - Plus
+baseurl=https://vault.centos.org/7.9.2009/centosplus/x86_64/
+gpgcheck=1
+enabled=0
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+EOF
+
+# kubernetes repo
+gg_pkg="packages.cloud.google.com/yum/doc" # Due to shorten addr for key
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.30/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.30/rpm/repodata/repomd.xml.key
+#exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
+EOF
+
+yum clean all
+
+# Set SELinux in permissive mode (effectively disabling it)
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+# RHEL/CentOS 7 have reported traffic issues being routed incorrectly due to iptables bypassed
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+EOF
+modprobe br_netfilter
+
+# local small dns & vagrant cannot parse and delivery shell code.
+echo "192.168.1.10 m-k8s" >> /etc/hosts
+for (( i=1; i<=$1; i++  )); do echo "192.168.1.10$i w$i-k8s" >> /etc/hosts; done
+
